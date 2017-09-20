6cf872583116fdc2dd2d25f1788efd88c1b60ecf
Jan Wytze <jwzuid@live.nl>
2017-09-20 16:42:48 +0200

    Added rule documentation

--- src/main/java/nl/janwytze/validation/rules/Confirmed.java
+++ src/main/java/nl/janwytze/validation/rules/Confirmed.java
@@ -5,16 +5,33 @@ import nl.janwytze.validation.Validator;
 
 public class Confirmed extends AbstractRule {
 
+    /**
+     * Confirmed constructor.
+     *
+     * @param parameters The rule parameter.
+     */
     public Confirmed(String[] parameters) {
         super(parameters);
     }
 
+    /**
+     * Validate the rule parameters.
+     *
+     * @throws IllegalArgumentException Thrown when the rule parameters are invalid.
+     */
     protected void validateParameters() throws IllegalArgumentException {
         if (parameters.length != 1) {
             throw new IllegalArgumentException();
         }
     }
 
+    /**
+     * Validate a certain value.
+     *
+     * @param validator The validator, this instance can be used to for cross-field validation.
+     * @param value The value to validate.
+     * @return False when failed.
+     */
     public boolean validate(Validator validator, Object value) {
         return (validator.getValue(this.parameters[0]).equals(value));
     }
--- src/main/java/nl/janwytze/validation/rules/NotNull.java
+++ src/main/java/nl/janwytze/validation/rules/NotNull.java
@@ -5,16 +5,33 @@ import nl.janwytze.validation.Validator;
 
 public class NotNull extends AbstractRule {
 
+    /**
+     * Confirmed constructor.
+     *
+     * @param parameters The rule parameter.
+     */
     public NotNull(String[] parameters) {
         super(parameters);
     }
 
+    /**
+     * Validate the rule parameters.
+     *
+     * @throws IllegalArgumentException Thrown when the rule parameters are invalid.
+     */
     protected void validateParameters() throws IllegalArgumentException {
         if (parameters.length != 0) {
             throw new IllegalArgumentException();
         }
     }
 
+    /**
+     * Validate a certain value.
+     *
+     * @param validator The validator, this instance can be used to for cross-field validation.
+     * @param value The value to validate.
+     * @return False when failed.
+     */
     public boolean validate(Validator validator, Object value) {
         return (value != null && value != "");
     }
